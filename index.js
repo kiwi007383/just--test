38acc3bc6255d1f622e0bf8eb7b8a6126abd8bbe
kiwi <kiwi007383@gmail.com>
2023-03-11 12:31:30 +0630

    adding newPersonsArray with filter method js

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const persons = people.map(person =>{
 });
 
 console.log(persons);
+
+const newPersons = [
+    {name: 'kiwi', age: 30, gender: 'male'},
+    {name: 'josh', age: 34, gender: 'male'},
+    {name: 'gray', age: 18, gender: 'female'}
+];
+
+const newPersonsArray = newPersons.filter(newPerson =>{
+    return newPerson.gender == 'male';
+});
+
+console.log(newPersonsArray);
